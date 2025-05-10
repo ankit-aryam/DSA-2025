8699aab0f78d1fcf8317fd7d8426bdafafd04ad3
Ankit <mandyarya2848@gmail.com>
2025-05-10 16:28:03 +0530

    Tree Traversal

--- genericTree.java
+++ genericTree.java
@@ -20,6 +20,51 @@ public class genericTree{
             display(child);
         }
     }
+
+    public static int size(Node node){
+        int size = 0;
+        for(Node child: node.children){
+            int cs = size(child);
+            size+=cs;
+        }
+        size++;
+        return size;
+    }
+
+    public static int maximum(Node node){
+        int max = node.data;
+
+        for(Node child: node.children){
+            int newMax = maximum(child);
+            max = Math.max(newMax,max);
+        }
+        max = Math.max(max, node.data);
+        return max;
+    }
+
+    public static int height(Node node){
+        int ht = -1;
+        for(Node child: node.children){
+            int ch = height(child);
+            ht = Math.max(ch, ht);
+        }
+        ht++;
+        return ht;
+    }
+
+    public static void traversal(Node node){
+
+        System.out.println("Pre Node:- " + node.data);
+
+        for(Node child:node.children){
+        System.out.println("Pre Edge:- " + child.data);
+        traversal(child);
+        System.out.println("Post Edge:- " + child.data);
+        }
+
+        System.out.println("Post Node:- " + node.data);
+
+    }
     public static void main(String[] args) {
         Node root=null;
 
@@ -46,6 +91,16 @@ public class genericTree{
             }
         }
         display(root);
+        int sz = size(root);
+        System.out.println("Size: "+sz);
+
+        int max = maximum(root);
+        System.out.println("Max: "+max);
+
+        int hgt = height(root);
+        System.out.println("Height: "+hgt);
+
+        traversal(root);
         
 
     }
